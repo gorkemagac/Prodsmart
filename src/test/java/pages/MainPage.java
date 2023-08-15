34526f38addde2b769a3760861241e9f9f317e46
Gorkem Agac <gorkem_agac@hotmail.com>
2023-08-16 00:00:31 +0100

    Prodsmart

---
+++ src/test/java/pages/MainPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class MainPage extends BasePage{
+    public MainPage()
+    {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy (xpath = "(//button[@class='button-get-started-reverse'])[3]")
+    public WebElement requestAFreeTrialButton;
+}
